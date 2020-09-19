8a4ecbab06c917ad5fbf47bb6331ec9977f98259
evanboardway <boardwae@mail.gvsu.edu>
2020-09-18 22:02:16 -0400

    gg

--- config/prod.exs
+++ config/prod.exs
@@ -21,16 +21,16 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-    config :beepbopskeet, BeepbopskeetWeb.Endpoint,
-      ...
-      url: [host: "example.com", port: 443],
-      https: [
-        port: 443,
-        cipher_suite: :strong,
-        keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-        certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-        transport_options: [socket_opts: [:inet6]]
-      ]
+    # config :beepbopskeet, BeepbopskeetWeb.Endpoint,
+    #   ...
+    #   url: [host: "example.com", port: 443],
+    #   https: [
+    #     port: 443,
+    #     cipher_suite: :strong,
+    #     keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+    #     certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
+    #     transport_options: [socket_opts: [:inet6]]
+    #   ]
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
