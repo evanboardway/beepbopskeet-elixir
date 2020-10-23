47877350ec6825fec1befd16a5428d72173c228a
evanboardway <boardwae@mail.gvsu.edu>
2020-10-23 15:28:06 -0400

    added links. changed styles

--- lib/beepbopskeet_web/views/email_view.ex
+++ lib/beepbopskeet_web/views/email_view.ex
@@ -3,8 +3,9 @@ defmodule BeepbopskeetWeb.EmailView do
 
   def get_category(followers) do
     cond do
-      followers > 20000 && followers <= 30000 -> 4
-      followers > 10000 && followers <= 20000 -> 3
+      followers > 20000 -> 5
+      followers > 15000 && followers <= 20000 -> 4
+      followers > 10000 && followers <= 15000 -> 3
       followers > 5000 && followers <= 10000 -> 2
       followers <= 5000 -> 1
     end
@@ -12,28 +13,31 @@ defmodule BeepbopskeetWeb.EmailView do
 
   def get_price_two_weeks(category) do
     case category do
-      1 -> "$25"
-      2 -> "$30"
-      3 -> "$65"
-      4 -> "$85"
+      1 -> "$10"
+      2 -> "$20"
+      3 -> "$30"
+      4 -> "$45"
+      5 -> "$50"
     end
   end
 
   def get_price_one_month(category) do
     case category do
-      1 -> "$35"
-      2 -> "$50"
-      3 -> "$100"
-      4 -> "$120"
+      1 -> "$20"
+      2 -> "$35"
+      3 -> "$45"
+      4 -> "$75"
+      5 -> "$85"
     end
   end
 
   def price_reduce(category) do
     case category do
-      1 -> "$5"
+      1 -> "$1"
       2 -> "$5"
       3 -> "$10"
       4 -> "$15"
+      5 -> "$15"
     end
   end
 end
